696533cbc441cab3285c40964d6b84a0b0767d91
junginsung <hunter10@naver.com>
2016-10-06 18:35:37 +0900

    Initial Commit

---
+++ git_xcode/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  git_xcode
+//
+//  Created by junginsung on 2016. 10. 6..
+//  Copyright © 2016년 junginsung. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
